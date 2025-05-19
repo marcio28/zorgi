f6563170347e098e5139cb514cac6919067dcb38
Marcio <marcio28@gmail.com>
2025-05-18 21:48:29 -0300

    Nova propriedade para testar validação de decimal.

--- src/zorgi.core/Models/Cuidador.cs
+++ src/zorgi.core/Models/Cuidador.cs
@@ -4,20 +4,28 @@
     {
         public string Nome { get; private set; }
         public string Email { get; private set; }
+        public decimal SalarioPorHora { get; private set; }
 
         // EF Relation
         protected Cuidador() { }
 
-        public Cuidador(Guid id, string nome, string email)
+        public Cuidador(Guid id, string nome, string email, decimal salarioPorHora)
         {
             Id = id;
             Nome = nome;
             Email = email;
+            SalarioPorHora = salarioPorHora;
         }
 
         public void TrocarEmail(string email)
         {
             Email = email;
         }
+
+        public void TrocarSalarioPorHora(decimal salarioPorHora)
+        {
+            SalarioPorHora = salarioPorHora;
+        }
+
     }
 }
