a195785745cede34ec4f5c0b90ea6ddb8268efbc
Marcio <marcio28@gmail.com>
2025-05-19 21:19:37 -0300

    EntityFramework configurado.

---
+++ src/zorgi.core/Configurations/CuidadorConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using zorgi.core.Models;
+
+namespace zorgi.core.Configurations
+{
+    public class CuidadorConfiguration : IEntityTypeConfiguration<Cuidador>
+    {
+        public void Configure(EntityTypeBuilder<Cuidador> builder)
+        {
+            builder.ToTable("Cuidadores");
+            builder.HasKey(c => c.Id);
+            builder.Property(c => c.Nome).IsRequired().HasMaxLength(100);
+            builder.Property(c => c.Email).IsRequired().HasMaxLength(100);
+            builder.Property(c => c.SalarioPorHora).IsRequired().HasColumnType("decimal(9,2)");
+        }
+    }
+}
--- src/zorgi.webapp.mvc/Program.cs
+++ src/zorgi.webapp.mvc/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using zorgi.core.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(o => o.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
