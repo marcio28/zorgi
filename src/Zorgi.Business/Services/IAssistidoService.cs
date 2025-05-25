fe9c5347a214ee0cb7e896a05a046901b21db59a
Marcio <marcio28@gmail.com>
2025-05-25 18:57:15 -0300

    Serviços inciais criados.

---
+++ src/Zorgi.Business/Services/IAssistidoService.cs
@@ -0,0 +1,11 @@
+﻿using Zorgi.Business.Models;
+
+namespace Zorgi.Business.Services
+{
+    public interface IAssistidoService : IDisposable
+    {
+        Task Adicionar(Assistido assistido);
+        Task Atualizar(Assistido assistido);
+        Task Remover(Guid id);
+    }
+}
---
+++ src/Zorgi.Business/Services/ICuidadorService.cs
@@ -0,0 +1,11 @@
+﻿using Zorgi.Business.Models;
+
+namespace Zorgi.Business.Services
+{
+    public interface ICuidadorService : IDisposable
+    {
+        Task Adicionar(Cuidador cuidador);
+        Task Atualizar(Cuidador cuidador);
+        Task Remover(Guid id);
+    }
+}
