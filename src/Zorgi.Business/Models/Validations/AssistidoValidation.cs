5e563f804c0b84a2549e527beebeed29ec13c4d5
Marcio <marcio28@gmail.com>
2025-05-25 18:27:35 -0300

    Validações iniciais criadas.

---
+++ src/Zorgi.Business/Models/Validations/AssistidoValidation.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace Zorgi.Business.Models.Validations
+{
+    public class AssistidoValidation : AbstractValidator<Assistido>
+    {
+        public AssistidoValidation()
+        {
+            ValidarNome();
+        }
+
+        private void ValidarNome()
+        {
+            RuleFor(a => a.Nome)
+                .NotEmpty().WithMessage("O campo {PropertyName} deve ser preenchido.")
+                .Length(2, 100)
+                .WithMessage("O campo {PropertyName} deve ter entre {MinLength} e {MaxLength} caracteres.");
+        }
+    }
+}
---
+++ src/Zorgi.Business/Models/Validations/CuidadorValidation.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace Zorgi.Business.Models.Validations
+{
+    public class CuidadorValidation : AbstractValidator<Cuidador>
+    {
+        public CuidadorValidation()
+        {
+            ValidarNome();
+        }
+
+        private void ValidarNome()
+        {
+            RuleFor(c => c.Nome)
+                .NotEmpty().WithMessage("O campo {PropertyName} deve ser preenchido.")
+                .Length(2, 100)
+                .WithMessage("O campo {PropertyName} deve ter entre {MinLength} e {MaxLength} caracteres.");
+        }
+    }
+}
