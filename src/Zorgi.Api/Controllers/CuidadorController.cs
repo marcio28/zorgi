770b4ce3dbd43a7bc0ce442de44df53795d8be25
Marcio <marcio28@gmail.com>
2025-06-01 18:26:59 -0300

    Criação de primeiros controllers.

---
+++ src/Zorgi.Api/Controllers/CuidadorController.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Zorgi.Api.ViewModels;
+using Zorgi.Business.Repositories;
+
+namespace Zorgi.Api.Controllers
+{
+    [ApiController]
+    [Route("api/cuidadores")]
+    public class CuidadorController(ICuidadorRepository cuidadorRepository, 
+                                    IMapper mapper) : MainController
+    {
+        private readonly ICuidadorRepository _cuidadorRepository = cuidadorRepository;
+        private readonly IMapper _mapper = mapper;
+
+        public async Task<IEnumerable<CuidadorViewModel>> ObterTodos()
+        {
+            var cuidadores = _mapper.Map<IEnumerable<CuidadorViewModel>>(await _cuidadorRepository.ObterTodos());
+            return cuidadores;
+        }
+    }
+}
---
+++ src/Zorgi.Api/Controllers/MainController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Zorgi.Api.Controllers
+{
+    [ApiController]
+    public abstract class MainController : ControllerBase 
+    {
+        
+    }
+}
